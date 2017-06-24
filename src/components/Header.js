365419521ac98fb021f718ad6eae69715090a192
SH-Stark <salmane.hajouji@me.com>
2017-06-24 19:53:22 +0200

    feat: added navigation with react router dom

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,32 @@
 import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+} from 'react-router-dom';
 import styles from '../App.css';
-
+import { Home, Events, Artists, LogIn, SignUp } from '../screens/index';
 
 class Header extends Component {
   render() {
     return (
-      <div className={styles.navContainer}>
-        <ul className={styles.header}>
-          <li className={styles.headerElement}>Logo</li>
-          <li className={styles.headerElement}>Events</li>
-          <li className={styles.headerElement}>Artists</li>
-          <li className={styles.headerElement}>Sign Up</li>
-          <li className={styles.headerElement}>Log In</li>
-        </ul>
-      </div>
+      <Router>
+        <div className={styles.navContainer}>
+          <ul className={styles.header}>
+            <li className={styles.headerElement}><Link to="/">Logo</Link></li>
+            <li className={styles.headerElement}><Link to="/events">Events</Link></li>
+            <li className={styles.headerElement}><Link to="/artists">Artists</Link></li>
+            <li className={styles.headerElement}><Link to="/signup">Sign Up</Link></li>
+            <li className={styles.headerElement}><Link to="/login">Log In</Link></li>
+          </ul>
+
+          <Route exact path="/" component={Home} />
+          <Route exact path="/events" component={Events} />
+          <Route exact path="/artists" component={Artists} />
+          <Route exact path="/signup" component={SignUp} />
+          <Route exact path="/login" component={LogIn} />
+        </div>
+      </Router>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route } from 'react-router';
 
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
-ReactDOM.render(
-  <Router history={Router.hashHistory}>
-    <Route path="/" component={App} />
-  </Router>,
-document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
