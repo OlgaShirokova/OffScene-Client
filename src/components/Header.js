e758f8447deaaaa0fbe52a798325c90edacde06e
SH-Stark <salmane.hajouji@me.com>
2017-06-24 16:57:25 +0200

    initial commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,8 +3,14 @@ import React, { Component } from 'react';
 class Header extends Component {
   render() {
     return (
-      <div>
-        Header
+      <div className="navContainer">
+        <ul className="header">
+          <li>Logo</li>
+          <li>Events</li>
+          <li>Artists</li>
+          <li>Sign Up</li>
+          <li>Log In</li>
+        </ul>
       </div>
     );
   }
