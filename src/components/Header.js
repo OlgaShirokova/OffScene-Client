465e92155d759420f57b497e70a6d8c5cce41e29
SH-Stark <salmane.hajouji@me.com>
2017-06-26 15:20:51 +0200

    refactor: delete Route, create Link component (reUsable)

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,31 +1,21 @@
 import React, { Component } from 'react';
-import {
-  BrowserRouter as Router,
-  Route,
-  Link,
-} from 'react-router-dom';
-import styles from '../App.css';
-import { Home, Events, Artists, LogIn, SignUp } from '../screens/index';
+import { BrowserRouter as Router, NavLink } from 'react-router-dom';
+import styles from './Header.css';
+
+const Link = (options) => (<NavLink activeClassName={styles.active} {...options}>
+  {options.title}</NavLink>);
 
 class Header extends Component {
   render() {
     return (
       <Router>
-        <div className={styles.navContainer}>
-          <ul className={styles.header}>
-            <li className={styles.headerElement}><Link to="/">Logo</Link></li>
-            <li className={styles.headerElement}><Link to="/events">Events</Link></li>
-            <li className={styles.headerElement}><Link to="/artists">Artists</Link></li>
-            <li className={styles.headerElement}><Link to="/signup">Sign Up</Link></li>
-            <li className={styles.headerElement}><Link to="/login">Log In</Link></li>
-          </ul>
-
-          <Route exact path="/" component={Home} />
-          <Route exact path="/events" component={Events} />
-          <Route exact path="/artists" component={Artists} />
-          <Route exact path="/signup" component={SignUp} />
-          <Route exact path="/login" component={LogIn} />
-        </div>
+        <ul className={styles.header}>
+          <li><Link to="/home" title="Logo" /></li>
+          <li><Link to="/events" title="Events" /></li>
+          <li><Link to="/artists" title="Artists" /></li>
+          <li><Link to="/signup" title="Sign Up" /></li>
+          <li><Link to="/login" title="Log In" /></li>
+        </ul>
       </Router>
     );
   }
