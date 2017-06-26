2e39c350b36c2707b3954a9c6d19ff196f190f33
SH-Stark <salmane.hajouji@me.com>
2017-06-27 00:17:01 +0200

    chore/style: header component with material UI

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import styles from './App.css';
 import Header from './components/Header';
 
 class App extends Component {
   render() {
     return (
-      <div className={styles.App}>
-        <Header />
-      </div>
+      <MuiThemeProvider>
+        <div className={styles.App}>
+          <Header />
+        </div>
+      </MuiThemeProvider>
     );
   }
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,73 @@
+import AppBar from 'material-ui/AppBar';
+import IconButton from 'material-ui/IconButton';
+import IconMenu from 'material-ui/svg-icons/navigation/menu';
+import Drawer from 'material-ui/Drawer';
+import MenuItem from 'material-ui/MenuItem';
+
 import React, { Component } from 'react';
 import { BrowserRouter as Router, NavLink } from 'react-router-dom';
 import styles from './Header.css';
 
-const Link = (options) => (<NavLink activeClassName={styles.active} {...options}>
-  {options.title}</NavLink>);
+const Link = options => (<NavLink activeClassName={styles.active} {...options}>
+  {options.title.toUpperCase()}</NavLink>);
 
 class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { open: false };
+    this.handleToggle = this.handleToggle.bind(this);
+    this.handleClose = this.handleClose.bind(this);
+    this.authentication = this.authentication.bind(this);
+    this.eventsArtists = this.eventsArtists.bind(this);
+  }
+
+  handleToggle() {
+    this.setState({ open: !this.state.open });
+  }
+
+  handleClose() {
+    this.setState({ open: false });
+  }
+
+  eventsArtists() {
+    return (
+      <div>
+        <MenuItem onTouchTap={this.handleClose}><Link to="/events" title="Events" /></MenuItem>
+        <MenuItem onTouchTap={this.handleClose}><Link to="/artists" title="Artists" /></MenuItem>
+      </div>
+    );
+  }
+
+  authentication() {
+    return (
+      <div>
+        <MenuItem onTouchTap={this.handleClose}><Link to="/signup" title="Sign Up" /></MenuItem>
+        <MenuItem onTouchTap={this.handleClose}><Link to="/login" title="Log In" /></MenuItem>
+      </div>
+    );
+  }
+
   render() {
     return (
       <Router>
-        <ul className={styles.header}>
-          <li><Link to="/home" title="Logo" /></li>
-          <li><Link to="/events" title="Events" /></li>
-          <li><Link to="/artists" title="Artists" /></li>
-          <li><Link to="/signup" title="Sign Up" /></li>
-          <li><Link to="/login" title="Log In" /></li>
-        </ul>
+        <div>
+          <AppBar
+            title={<a className={styles.title}>OFFSTAGE</a>}
+            style={{ backgroundColor: 'white' }}
+            iconElementLeft={<IconButton><IconMenu onTouchTap={this.handleToggle} /></IconButton>}
+          />
+          <Drawer
+            docked={false}
+            width={100}
+            open={this.state.open}
+            onRequestChange={open => this.setState({ open })}
+          >
+            <MenuItem onTouchTap={this.handleClose}><Link to="/home" title="Home" /></MenuItem>
+            {this.eventsArtists()}
+            {this.authentication()}
+          </Drawer>
+
+        </div>
       </Router>
     );
   }
