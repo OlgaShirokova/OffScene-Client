84948afd1b1f950f8754492ca80773f9cb2fb2e2
David Casanellas <davidcasanell@gmail.com>
2017-06-26 10:51:49 +0200

    chore: add material-ui and react-tap-event-plugin

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
+import injectTapEventPlugin from 'react-tap-event-plugin';
 import styles from './App.css';
 import Header from './components/Header';
+
+injectTapEventPlugin();
 
 class App extends Component {
   render() {
