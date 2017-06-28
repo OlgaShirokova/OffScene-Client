ce7ebdeb5bc412a67256a265e6f9ecc9f00be477
David Casanellas <davidcasanell@gmail.com>
2017-06-28 12:14:40 +0200

    feat: added date and genre filters. fix: linter babel-parser

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,11 +1,69 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import DatePicker from 'material-ui/DatePicker';
+import AutoComplete from 'material-ui/AutoComplete';
 
 class Filter extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      dataSource: [],
+    };
+  }
+
+  handleUpdateInput = (value) => {
+    this.setState({
+      dataSource: [
+        value,
+        value + value,
+        value + value + value,
+      ],
+    });
+  };
+
+  renderFilterContent() {
+    if (this.props.type === 'date') {
+      return (
+        <div>
+          <DatePicker hintText="Portrait Dialog" mode="landscape" />
+          <DatePicker hintText="Landscape Dialog" mode="landscape" />
+        </div>
+      );
+    } else if (this.props.type === 'genre') {
+      return (
+        <div>
+          <AutoComplete
+            hintText="Type anything"
+            dataSource={this.state.dataSource}
+            onUpdateInput={this.handleUpdateInput}
+          />
+        </div>
+      );
+    }
+
+    return (
+      <div>
+        This is a price filter
+      </div>
+    );
+  }
+
   render() {
     return (
-      <div>A filter box</div>
+      <div>
+        {this.props.type.charAt(0).toUpperCase() + this.props.type.slice(1)}
+        {this.renderFilterContent()}
+      </div>
     );
   }
 }
+
+Filter.propTypes = {
+  type: PropTypes.string.isRequired,
+};
+
 
 export default Filter;
--- src/containers/Filters.js
+++ src/containers/Filters.js
@@ -1,14 +1,23 @@
 import React, { Component } from 'react';
 import Filter from '../components/Filter';
 
+const styles = {
+  filters: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    height: 300,
+    backgroundColor: 'red',
+  },
+};
+
 class Filters extends Component {
+
   render() {
     return (
-      <div>
-        <div>Filters</div>
-        <Filter />
-        <Filter />
-        <Filter />
+      <div style={styles.filters}>
+        <Filter type='date' />
+        <Filter type='genre' />
+        <Filter type='price' />
       </div>
     );
   }
