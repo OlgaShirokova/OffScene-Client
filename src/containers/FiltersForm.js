b83b2c92fcd474063ccc07732cd1181618a945c5
David Casanellas <davidcasanell@gmail.com>
2017-06-29 10:17:45 +0200

    style: filters in two rows

--- src/containers/FiltersForm.js
+++ src/containers/FiltersForm.js
@@ -15,38 +15,39 @@ class FiltersForm extends Component {
   render() {
     return (
       <form className={styles.formContainer}>
-        {/* <div className={styles.date}> */}
-        <div>
-          <Field
-            name="date"
-            component={DatePicker}
-            format={null}
-            floatingLabelText="What day is the event?"
-          />
+        <div className={styles.formContainerTopRow}>
+          <div className={styles.date}>
+            <Field
+              name="date"
+              component={DatePicker}
+              format={null}
+              floatingLabelText="What day is the event?"
+            />
+          </div>
+          <div className={styles.price}>
+            <Field
+              name="price"
+              component={SelectField}
+              floatingLabelText="Select a plan"
+            >
+              <MenuItem value="0 - 500$" primaryText="0 - 500$" />
+              <MenuItem value="500$ - 2000$" primaryText="500$ - 2000$" />
+              <MenuItem value="2000$ - 5000$" primaryText="2000$ - 5000$" />
+              <MenuItem value="+5000$" primaryText="+5000$" />
+            </Field>
+          </div>
         </div>
-        {/* <div className={styles.genres}> */}
         <div>
-          <Field
-            name="genre"
-            component={AutoComplete}
-            floatingLabelText="Select some music genres"
-            openOnFocus
-            filter={MUIAutoComplete.fuzzyFilter}
-            dataSource={dataSource}
-          />
-        </div>
-        {/* <div className={styles.price}> */}
-        <div>
-          <Field
-            name="price"
-            component={SelectField}
-            floatingLabelText="Select a plan"
-          >
-            <MenuItem value="0 - 500$" primaryText="0 - 500$" />
-            <MenuItem value="500$ - 2000$" primaryText="500$ - 2000$" />
-            <MenuItem value="2000$ - 5000$" primaryText="2000$ - 5000$" />
-            <MenuItem value="+5000$" primaryText="+5000$" />
-          </Field>
+          <div className={styles.genres}>
+            <Field
+              name="genre"
+              component={AutoComplete}
+              floatingLabelText="Select some music genres"
+              openOnFocus
+              filter={MUIAutoComplete.fuzzyFilter}
+              dataSource={dataSource}
+            />
+          </div>
         </div>
       </form>
     );
