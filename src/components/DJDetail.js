f01fc59f60a97daaec26d499a524bf676b6c8089
David Casanellas <davidcasanell@gmail.com>
2017-07-01 18:59:07 +0200

    fix: replacing mockdata with data from state

--- src/components/DJDetail.js
+++ src/components/DJDetail.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router';
 import { connect } from 'react-redux';
-import { func } from 'prop-types';
+import { object } from 'prop-types';
 import { LocationOn } from 'material-ui-icons';
 import Subheader from 'material-ui/Subheader';
 import RaisedButton from 'material-ui/RaisedButton';
@@ -15,57 +15,20 @@ import {
 import {
   BrowserRouter as Router,
 } from 'react-router-dom';
-import { getDJ } from '../actions';
 import styles from './DJDetail.css';
 import { Facebook, Twitter, Youtube, Instagram, Soundcloud, Spotify } from '../icons/Icons';
 
-const currentDJId = location.pathname.split('/')[2];
-const mockDJ = {
-  id: 124,
-  name: 'Donkey Kong',
-  picture: 'https://avatars1.githubusercontent.com/u/1481696?v=3&s=400',
-  calendar: {
-    monday: 0,
-    tuesday: 0,
-    wednesday: 1,
-    thursday: 0,
-    friday: 1,
-    saturday: 1,
-    sunday: 1,
-  },
-  awayDays: [
-    '2017-06-30T00:00:00+00:00',
-    '2017-07-05T00:00:00+00:00',
-    '2017-07-13T00:00:00+00:00',
-  ],
-  priceWe: 1000000,
-  priceWd: 500000,
-  city: 'Barcelona',
-  lat: 33.0684,
-  long: 34.0567,
-  avgRating: 400,
-  musicGenres: [
-    'rap',
-    'techno',
-  ],
-};
-
 class DJDetail extends Component {
 
-  componentWillMount() {
-    this.props.getDJ(currentDJId);
-  }
-
   render() {
-    console.log('mockdj ', mockDJ);
     return (
       <Router>
         <div className={styles.djDetailContainer}>
           <Card className={styles.card}>
             <CardHeader
-              title={mockDJ.name}
-              subtitle={`Rating: ${mockDJ.avgRating / 100}/5`}
-              avatar={mockDJ.picture}
+              title={this.props.djs[this.props.match.params.id].name}
+              subtitle={`Rating: ${this.props.djs[this.props.match.params.id].avgRating / 100}/5`}
+              avatar={this.props.djs[this.props.match.params.id].picture}
               titleStyle={{ fontWeight: '600' }}
               subtitleStyle={{ fontWeight: '100' }}
             />
@@ -73,7 +36,7 @@ class DJDetail extends Component {
             <Subheader>
               <div className={styles.city}>
                 <LocationOn />
-                <Subheader>{mockDJ.city}</Subheader>
+                <Subheader>{this.props.djs[this.props.match.params.id].city}</Subheader>
               </div>
             </Subheader>
             <Divider />
@@ -90,14 +53,12 @@ class DJDetail extends Component {
             </div>
             <Divider />
             <CardActions>
-              {/* <Link to={`/artists/${'this.props.dj.id'}`} > */}
               <RaisedButton
                 label="SEE CONTRACT"
                 primary={!0}
                 fullWidth={!0}
               />
               <Divider className={styles.divider} />
-              {/* </Link> */}
               <RaisedButton
                 label="BOOK NOW!"
                 primary={!0}
@@ -105,18 +66,15 @@ class DJDetail extends Component {
               />
             </CardActions>
           </Card>
-          {/* </div>
-          <div className={styles.paperContainer}>
-          <Paper className={styles.paper}> */}
           <Paper className={styles.paper}>
-            <div style={{ paddingLeft: '16', paddingBottom: '9' }}>
+            <div style={{ paddingLeft: 16, paddingBottom: 9 }}>
               <Subheader style={{ padding: 0, fontWeight: '100' }}>
                 Pictures and Videos
               </Subheader>
               {'Put some images here'}
             </div>
             <Divider />
-            <div style={{ paddingLeft: '16', paddingBottom: '9' }}>
+            <div style={{ paddingLeft: 16, paddingBottom: 9 }}>
               <Subheader style={{ padding: 0, fontWeight: '100' }}>
                 Events
               </Subheader>
@@ -131,14 +89,12 @@ class DJDetail extends Component {
 }
 
 DJDetail.propTypes = {
-  getDJ: func.isRequired,
+  match: object.isRequired,
+  djs: object.isRequired,
 };
 
-const mapStateToProps = () => {
-};
-
-const mapDispatchToProps = (dispatch) => ({
-  getDJ: (currentDJId) => dispatch(getDJ(currentDJId)),
+const mapStateToProps = (state) => ({
+  djs: state.entities.djs,
 });
 
-export default withRouter(connect(mapStateToProps, mapDispatchToProps)(DJDetail));
+export default withRouter(connect(mapStateToProps)(DJDetail));
