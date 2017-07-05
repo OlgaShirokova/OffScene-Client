5e6373d5b7648d0dd6a557230d2bc171cde2217c
aerox <aerox.javascript@hotmail.com>
2017-07-05 16:43:45 +0200

    refactor: update header to take into account auth state

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,18 +1,25 @@
 import React, { Component } from 'react';
 import AppBar from 'material-ui/AppBar';
+import { connect } from 'react-redux';
 import IconButton from 'material-ui/IconButton';
 import IconMenu from 'material-ui/svg-icons/navigation/menu';
 import Drawer from 'material-ui/Drawer';
 import MenuItem from 'material-ui/MenuItem';
 import { NavLink } from 'react-router-dom';
 import styles from './styles.css';
+import * as ActionCreators from 'actions';
 
 const Link = options =>
   <NavLink activeClassName={styles.active} {...options}>
     {options.title.toUpperCase()}
   </NavLink>;
 
+@connect(data => Header.getData, ActionCreators)
 export default class Header extends Component {
+  static getData = ({ auth }) => ({
+    isAuth: !!auth.id,
+  });
+
   state = {
     open: false,
   };
@@ -25,6 +32,39 @@ export default class Header extends Component {
     this.setState({ open: false });
   };
 
+  _renderMenuItemsAuth() {
+    return [
+      <MenuItem onTouchTap={this._handleClose}>
+        <Link to="/events" title="Events" />
+      </MenuItem>,
+      <MenuItem onTouchTap={this._handleClose}>
+        <a
+          onClick={event => {
+            event.preventDefault();
+            this._handleLogout();
+          }}
+        >
+          LOG OUT
+        </a>
+      </MenuItem>,
+    ];
+  }
+
+  _renderMenuItemsNoAuth() {
+    return [
+      <MenuItem onTouchTap={this._handleClose}>
+        <Link to="/signup" title="Sign Up" />
+      </MenuItem>,
+      <MenuItem onTouchTap={this._handleClose}>
+        <Link to="/login" title="Log In" />
+      </MenuItem>,
+    ];
+  }
+
+  _handleLogout = () => {
+    this.props.signOut();
+  };
+
   _renderMenuItems() {
     return (
       <div>
@@ -32,17 +72,11 @@ export default class Header extends Component {
           <Link exact to="/" title="Home" />
         </MenuItem>
         <MenuItem onTouchTap={this._handleClose}>
-          <Link to="/events" title="Events" />
-        </MenuItem>
-        <MenuItem onTouchTap={this._handleClose}>
           <Link to="/artists" title="Artists" />
         </MenuItem>
-        <MenuItem onTouchTap={this._handleClose}>
-          <Link to="/signup" title="Sign Up" />
-        </MenuItem>
-        <MenuItem onTouchTap={this._handleClose}>
-          <Link to="/login" title="Log In" />
-        </MenuItem>
+        {this.props.isAuth
+          ? this._renderMenuItemsAuth()
+          : this._renderMenuItemsNoAuth()}
       </div>
     );
   }
