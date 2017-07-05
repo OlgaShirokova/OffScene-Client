13643df66ad2139cb7b5c6b81d7d0f45f4b0b146
aerox <aerox.javascript@hotmail.com>
2017-07-05 16:44:52 +0200

    refactor: update rootReducer to take into account logout

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,24 @@
 import { combineReducers } from 'redux';
+import types from 'actions/types';
 import { reducer as formReducer } from 'redux-form';
 import entities from './entitiesReducer';
 import pages from './pagesReducer';
+import { deleteToken } from 'utils/localStorage';
+import auth from './authReducer';
 
-export default combineReducers({
+const reducers = combineReducers({
+  auth,
   entities,
   form: formReducer,
   pages,
 });
+
+function rootReducer(state, action) {
+  if (action.type === types.SIGN_OUT) {
+    state = {};
+    deleteToken();
+  }
+  return reducers(state, action);
+}
+
+export default rootReducer;
