859c3ba2fc7515b815d05a6bfd463a9cf02c8b5d
aerox <aerox.javascript@hotmail.com>
2017-07-05 18:22:11 +0200

    refactor: SignupPage

--- src/pages/SignupPage/index.js
+++ src/pages/SignupPage/index.js
@@ -4,102 +4,124 @@ import { Card } from 'material-ui/Card';
 import RaisedButton from 'material-ui/RaisedButton';
 import TextField from 'material-ui/TextField';
 import styles from './styles.css';
+import { connect } from 'react-redux';
+import * as ActionCreators from 'actions';
+import { Redirect } from 'react-router';
 
-const SignUpForm = ({ onSubmit, onChange, errors, user }) =>
-  <Card className={styles.container}>
-    <form action="/" onSubmit={onSubmit}>
-      <h2 className={styles.subtitle}>Sign Up</h2>
+function SignupForm({ onSubmit, onChange, errors, user }) {
+  return (
+    <Card className={styles.container}>
+      <form action="/" onSubmit={onSubmit}>
+        <h2 className={styles.subtitle}>Sign Up</h2>
 
-      {errors.summary &&
-        <p className="error-message">
-          {errors.summary}
-        </p>}
+        {errors &&
+          <p className={styles.errorMsg}>
+            {errors}
+          </p>}
 
-      <div className={styles.fieldLine}>
-        <TextField
-          floatingLabelText="Email"
-          name="email"
-          errorText={errors.email}
-          onChange={onChange}
-          value={user.email}
-        />
-      </div>
+        <div className={styles.fieldLine}>
+          <TextField
+            floatingLabelText="Email"
+            type="email"
+            name="email"
+            onChange={onChange}
+            errorText={errors && ' '}
+            value={user.email}
+          />
+        </div>
 
-      <div className={styles.fieldLine}>
-        <TextField
-          floatingLabelText="Password"
-          type="password"
-          name="password"
-          onChange={onChange}
-          errorText={errors.password}
-          value={user.password}
-        />
-      </div>
+        <div className={styles.fieldLine}>
+          <TextField
+            floatingLabelText="Password"
+            type="password"
+            name="password"
+            onChange={onChange}
+            errorText={errors && ' '}
+            value={user.password}
+          />
+        </div>
 
-      <div className={styles.signupButton}>
-        <input
-          className={styles.choiceButton}
-          type="radio"
-          name="role"
-          value="0"
-          checked
-        />
-        <p>DJ</p>
-        <input
-          className={styles.choiceButton}
-          type="radio"
-          name="role"
-          value="1"
-        />
-        <p>Organizer</p>
-      </div>
+        <div className={styles.signupButton}>
+          <input
+            className={styles.choiceButton}
+            type="radio"
+            name="role"
+            value="0"
+            onChange={onChange}
+            checked={user.role == 0}
+          />
+          <p>DJ</p>
+          <input
+            className={styles.choiceButton}
+            type="radio"
+            name="role"
+            onChange={onChange}
+            value="1"
+            checked={user.role == 1}
+          />
+          <p>Organizer</p>
+        </div>
 
-      <div className={styles.signupButton}>
-        <RaisedButton type="submit" label="Sign Up" default />
-      </div>
-    </form>
-  </Card>;
+        <div className={styles.signupButton}>
+          <RaisedButton
+            className={styles.innerSignup}
+            type="submit"
+            label="Sign Up"
+            default
+          />
+        </div>
+      </form>
+    </Card>
+  );
+}
 
-SignUpForm.propTypes = {
-  onSubmit: PropTypes.func.isRequired,
-  onChange: PropTypes.func.isRequired,
-  errors: PropTypes.object.isRequired,
-  user: PropTypes.object.isRequired,
-};
-
+@connect(data => SignupPage.getData, ActionCreators)
 export default class SignupPage extends Component {
-  state = {
-    errors: {},
-    user: {
-      email: '',
-      password: '',
-    },
+  static getData = ({
+    pages: { signupPage: { form, loading, success } },
+    auth: { isAuth, isAuthenticating },
+  }) => ({
+    isAuth,
+    isAuthenticating,
+    form,
+    loading,
+    success,
+  });
+
+  _handleChange = ({ target: { name, value } }) => {
+    this.props.signupPageFormChange({ [name]: value });
   };
 
-  processForm = event => {
+  _handleSubmit = event => {
+    const { email, password, role } = this.props.form;
     event.preventDefault();
 
-    console.log('email:', this.state.user.email);
-    console.log('password:', this.state.user.password);
+    this.props.signUp({ email, password, role: Number(role) });
   };
 
-  changeUser(event) {
-    const field = event.target.name;
-    const user = this.state.user;
-    user[field] = event.target.value;
-
-    this.setState({
-      user,
-    });
-  }
-
   render() {
+    const {
+      loading,
+      success,
+      isAuth,
+      isAuthenticating,
+      form: { errors, ...user },
+    } = this.props;
+
+    if (success) {
+      return <Redirect to="/login" />;
+    }
+
+    if (loading) {
+      return <div>Loading...</div>;
+    }
+
     return (
-      <SignUpForm
-        onSubmit={this.processForm}
-        onChange={this.changeUser}
-        errors={this.state.errors}
-        user={this.state.user}
+      <SignupForm
+        onSubmit={this._handleSubmit}
+        onChange={this._handleChange}
+        errors={errors}
+        user={user}
       />
     );
   }
