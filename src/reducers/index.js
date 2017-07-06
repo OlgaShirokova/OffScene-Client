ec3e6d6ab7b7d602d6614714b279dea0fda32712
aerox <aerox.javascript@hotmail.com>
2017-07-06 08:48:27 +0200

    refactor: update reducers file to clean just the necessary state

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,19 +3,34 @@ import types from 'actions/types';
 import { reducer as formReducer } from 'redux-form';
 import entities from './entitiesReducer';
 import pages from './pagesReducer';
+import notification from './notificationReducer';
 import { deleteToken } from 'utils/localStorage';
 import auth from './authReducer';
+import {
+  SIGNUP_PAGE_INITIAL_STATE,
+  LOGIN_PAGE_INITIAL_STATE,
+} from './pagesReducer';
 
 const reducers = combineReducers({
   auth,
   entities,
   form: formReducer,
   pages,
+  notification,
 });
 
 function rootReducer(state, action) {
   if (action.type === types.SIGN_OUT) {
-    state = {};
+    state = {
+      ...state,
+      auth: undefined,
+      entities: { ...state.entities, events: {} },
+      pages: {
+        ...state.pages,
+        loginPage: LOGIN_PAGE_INITIAL_STATE,
+        signupPage: SIGNUP_PAGE_INITIAL_STATE,
+      },
+    };
     deleteToken();
   }
   return reducers(state, action);
