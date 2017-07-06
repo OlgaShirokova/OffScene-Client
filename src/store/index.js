a8826be7238842a0c171e9026956d20930e5fdc0
aerox <aerox.javascript@hotmail.com>
2017-07-06 09:20:40 +0200

    feat: conect redux-persist with the store

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,7 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import logger from 'redux-logger';
+import { autoRehydrate } from 'redux-persist';
+import { persistStoreAsync, purgeStoreAsync } from 'utils/reduxPersistWrapper';
 import reducers from 'reducers';
 const defaultConfig = {
   basePath: 'http://localhost:8000',
@@ -8,7 +10,12 @@ import apiMiddleware from 'middlewares/apiMiddleware';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-export default createStore(
+const store = createStore(
   reducers,
-  composeEnhancers(applyMiddleware(apiMiddleware(defaultConfig), logger))
+  composeEnhancers(applyMiddleware(apiMiddleware(defaultConfig), logger)),
+  autoRehydrate()
 );
+
+persistStoreAsync(store);
+
+export default store;
