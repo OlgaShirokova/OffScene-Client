0cc66ea709786e0e120db3b310d4ef561eb2cbb4
SH-Stark <salmane.hajouji@me.com>
2017-07-06 22:10:57 +0200

    style: added two new pictures for home

--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -3,6 +3,7 @@ import { GridList, GridTile } from 'material-ui/GridList';
 import IconButton from 'material-ui/IconButton';
 import StarBorder from 'material-ui/svg-icons/toggle/star-border';
 import imageTable from 'assets/table.jpeg';
+import imageDj from 'assets/solodj.jpeg';
 import styles from './styles.css';
 import { connect } from 'react-redux';
 import { object } from 'prop-types';
@@ -22,7 +23,10 @@ export default class HomePage extends Component {
     console.log(this.props);
     return (
       <div>
-        <img src={imageTable} style={{ maxWidth: '100%' }} alt="" />
+        <div className={styles.imgs}>
+          <img className={styles.imgHome} src={imageTable} alt="" />
+          <img className={styles.imgHome} src={imageDj} alt="" />
+        </div>
         <div className={options.root}>
           <h4 className={styles.subtitle}>NEAREST DJS AROUND YOU</h4>
           <GridList style={options.gridList} padding={0} cols={2.2}>
